e2fa1c834d94e3ea900bcb7dd2505347e86d2640
Yarli Rabelo <yrish@Yarlis-MacBook-Pro.local>
2024-12-10 22:14:17 +0000

    Setting express framework

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var http = require("http");
 
-http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
-}).listen(8081, function() {
-    console.log("Node server is running...");
-});
+const express = require("express");
+const app = express();
+const port = 8083;
+
+app.get("/", (req, res) => res.send("<h1>"+dt.myDateTime()+"</h1>"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
